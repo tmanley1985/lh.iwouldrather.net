20031d5f382e4dddf76595a0acf841d19ab1ed2d
tmanley1985 <tmanley1985@gmail.com>
2015-06-07 20:05:00 -0400

    Added function to compile sass

--- wp-content/themes/iwr/gulpfile.js
+++ wp-content/themes/iwr/gulpfile.js
@@ -1,27 +1,48 @@
 var gulp = require('gulp');
+var minify = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var es = require('event-stream');
+var sass = require('gulp-sass');
 // Command --save-dev will update your json file
+// 
 
+var config = {
+   sassPath: 'src/sass',
+   bowerDir: 'vendor/bower'
+}
 // Call Gulp scripts to call this function
-gulp.task('scripts', function(){
-	// Always return streams, Gulp uses this to know when this task has ran
-	return gulp.src('src/*.js')
-	//Pipe in various commands in chron order
-	//Concat param needs name for concatenated file
-	.pipe('concat('all.min.js')')
-	.pipe(uglify())
-	.pipe(gulp.dest('js'));
+// gulp.task('scripts', function(){
+	
+// 	var bootstrap = gulp.src('vendor/bower/boostrap-sass-official');
+
+// 	var mainjs = gulp.src('');
+// 	//Merge separate sources into on file
+// 	//Pipe in various commands in chron order
+// 	//Concat param needs name for concatenated file
+// 	return es.merge('bootstrap, mainjs')
+// 	.pipe(concat('app.min.js'))
+// 	.pipe(uglify())
+// 	.pipe(gulp.dest('js'));
+// });
+gulp.task('icons', function() {
+    return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*')
+        .pipe(gulp.dest('fonts'));
 });
 
+gulp.task('css', function() {
+    return gulp.src('src/sass/iwr.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'));
+});
 
 // Call Gulp styles to call this function
 gulp.task('styles', function(){
 	// Always return streams, Gulp uses this to know when this task has ran
-	return gulp.src('src/*.css')
 	//Pipe in various commands in chron order
 	//Concat param needs name for concatenated file
-	.pipe('concat('all.min.css')')
-	.pipe(uglify())
+	return gulp.src('src/css/*.css')
+	.pipe(concat('app.min.css'))
+	.pipe(minify())
 	.pipe(gulp.dest('css'));
 });
