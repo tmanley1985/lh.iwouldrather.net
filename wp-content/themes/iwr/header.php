efa0c785dec74ee1d1d0b7d72ec02f7c755caf07
tmanley1985 <tmanley1985@gmail.com>
2015-06-09 17:56:58 -0400

    Added a container div

--- wp-content/themes/iwr/header.php
+++ wp-content/themes/iwr/header.php
@@ -3,15 +3,25 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<link rel="stylesheet" href="">
+	<title>I Would Rather</title>
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
+	<?php wp_head(); ?>
 </head>
 <body>
-	<header id="header" class="">
-		<nav>
-			<ul>
-				<li><a href="" title="">I Would Rather</a></li>
-			</ul>
-		</nav>
-		
-	</header><!-- /header -->
+
+<nav class="navbar hidden-tablet hidden-phone">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      	<a class="navbar-brand" href="#">I Would Rather</a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Page 1</a></li>
+        <li><a href="#">Page 2</a></li> 
+        <li><a href="#">Page 3</a></li> 
+      </ul>
+    </div>
+  </div>
+</nav>
+	<div id="main-container" class="container">
