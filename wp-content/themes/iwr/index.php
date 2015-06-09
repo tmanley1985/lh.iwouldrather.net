114d40a5223b7325c3426b00e3e8c4ef5b127962
tmanley1985 <tmanley1985@gmail.com>
2015-06-09 17:55:36 -0400

    Added some content in the wordpress loop

--- wp-content/themes/iwr/index.php
+++ wp-content/themes/iwr/index.php
@@ -1,13 +1,30 @@
 <?php get_header(); ?>
 
+<div class="jumbotron text-center">
+		<h1>I Would Rather</h1>
+		<p>I review movies that I hate.</p>
+</div>
+
 <?php if ( have_posts() ) { ?>
 
 <?php while ( have_posts() ) {
 
 	the_post();
-	 the_content();
-	
 ?>
+	
+	<div class="row text-center blog-post">
+		<div class="col-sm-12">
+			<h1><?php echo the_title(); ?></h1>
+		</div>
+		<div class="col-sm-12">
+			<p><?php echo the_content(); ?></p>
+			<span><?php echo the_date('F,j,Y'); ?></span>
+		</div>
+	</div>
+
+	
+	
+
 
 <?php } ?>
 
