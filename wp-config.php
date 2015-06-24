e96350ef7625f7a5d6ba783da8a6509d25678980
tmanley1985 <tmanley1985@gmail.com>
2015-06-24 10:34:11 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'iwouldrather');
 
 /** MySQL database username */
-define('DB_USER', 'tmanley1985');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'pi3141592');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'lh.iwouldrather.net');
